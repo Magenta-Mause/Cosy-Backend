8fbf7b315e162fb19361cb0f9eb13514229d8fbb
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-12-30 14:42:45 +0100

    chore: fix linting

--- src/main/java/com/magentamause/cosybackend/controllers/GameApiController.java
+++ src/main/java/com/magentamause/cosybackend/controllers/GameApiController.java
@@ -2,17 +2,14 @@ package com.magentamause.cosybackend.controllers;
 
 import com.magentamause.cosybackend.dtos.entitydtos.GameDto;
 import com.magentamause.cosybackend.services.GamesApiService;
-import java.util.List;
-
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
