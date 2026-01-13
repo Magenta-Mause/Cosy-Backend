ea9300d83b0fae73d10c4627882fe0c0106bdba8
janne6565 <jabbekeipert@gmail.com>
2026-01-13 21:10:01 +0100

    chore: fix lint

--- src/main/java/com/magentamause/cosybackend/configs/properties/CorsProperties.java
+++ src/main/java/com/magentamause/cosybackend/configs/properties/CorsProperties.java
@@ -1,12 +1,7 @@
 package com.magentamause.cosybackend.configs.properties;
 
 import java.util.List;
-
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "cosy.cors")
-public record CorsProperties(List<String> allowedOrigins) {
-}
+public record CorsProperties(List<String> allowedOrigins) {}
