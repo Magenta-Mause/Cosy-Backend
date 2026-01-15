f6d1ab871ddaca58de528e7c8e49b3a2ce4b29e6
py-bay <github.com.tribunal325@passmail.net>
2026-01-15 21:20:44 +0100

    refactor: replace DTO classes with Java records for immutability and simplicity

--- src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerDockerProgressUpdateDto.java
+++ src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerDockerProgressUpdateDto.java
@@ -1,12 +1,6 @@
 package com.magentamause.cosybackend.dtos.websockets;
 
 import com.magentamause.cosybackend.dtos.entitydtos.PullProgressDto;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@Data
-@RequiredArgsConstructor
-public class GameServerDockerProgressUpdateDto {
-    private final String serverUuid;
-    private final PullProgressDto progress;
+public record GameServerDockerProgressUpdateDto(String serverUuid, PullProgressDto progress) {
 }
--- src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerStatusUpdateDto.java
+++ src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerStatusUpdateDto.java
@@ -1,12 +1,6 @@
 package com.magentamause.cosybackend.dtos.websockets;
 
 import com.magentamause.cosybackend.dtos.entitydtos.GameServerDto;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@Data
-@RequiredArgsConstructor
-public class GameServerStatusUpdateDto {
-    private final String serverUuid;
-    private final GameServerDto.GameServerStatus newStatus;
+public record GameServerStatusUpdateDto(String serverUuid, GameServerDto.GameServerStatus newStatus) {
 }
