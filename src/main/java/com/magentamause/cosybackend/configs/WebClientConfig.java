d853b0e5c1a75abe52faae05398910f6d91e9ebe
py-bay <github.com.tribunal325@passmail.net>
2026-01-15 19:26:04 +0100

    refactor: move WebClientConfig to configs package

--- src/main/java/com/magentamause/cosybackend/configs/WebClientConfig.java
+++ src/main/java/com/magentamause/cosybackend/configs/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.magentamause.cosybackend.configs.websockets;
+package com.magentamause.cosybackend.configs;
 
 import com.magentamause.cosybackend.configs.properties.GamesApiProperties;
 import com.magentamause.cosybackend.configs.properties.LokiProperties;
