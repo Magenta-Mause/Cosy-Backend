822c55420a3488b65b6993c1fc3bec879c239f3e
py-bay <github.com.tribunal325@passmail.net>
2026-01-15 21:21:54 +0100

    refactor: reformat code to improve readability in GameServerStatusUpdateDto and WebSocketDestinations

--- src/main/java/com/magentamause/cosybackend/configs/websockets/WebSocketDestinations.java
+++ src/main/java/com/magentamause/cosybackend/configs/websockets/WebSocketDestinations.java
@@ -12,8 +12,11 @@ public final class WebSocketDestinations {
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static final class Topics {
-        public static final String GAME_SERVER_LOGS_CREATION = BROKER_PREFIX + "/game-server-logs/creation/{serverId}";
-        public static final String GAME_SERVER_STATUS = BROKER_PREFIX + "/game-servers/status/{serverId}";
-        public static final String GAME_SERVER_DOCKER_PROGRESS = BROKER_PREFIX + "/game-servers/docker-progress/{serverId}";
+        public static final String GAME_SERVER_LOGS_CREATION =
+                BROKER_PREFIX + "/game-server-logs/creation/{serverId}";
+        public static final String GAME_SERVER_STATUS =
+                BROKER_PREFIX + "/game-servers/status/{serverId}";
+        public static final String GAME_SERVER_DOCKER_PROGRESS =
+                BROKER_PREFIX + "/game-servers/docker-progress/{serverId}";
     }
 }
--- src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerStatusUpdateDto.java
+++ src/main/java/com/magentamause/cosybackend/dtos/websockets/GameServerStatusUpdateDto.java
@@ -2,5 +2,5 @@ package com.magentamause.cosybackend.dtos.websockets;
 
 import com.magentamause.cosybackend.dtos.entitydtos.GameServerDto;
 
-public record GameServerStatusUpdateDto(String serverUuid, GameServerDto.GameServerStatus newStatus) {
-}
+public record GameServerStatusUpdateDto(
+        String serverUuid, GameServerDto.GameServerStatus newStatus) {}
