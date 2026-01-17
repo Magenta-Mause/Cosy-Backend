b65f2de84f53d91f8ecdb962761a216ca3f34963
Janne Keipert <jabbekeipert@gmail.com>
2026-01-17 02:41:12 +0100

    chore: fix linting

--- src/main/java/com/magentamause/cosybackend/exceptions/docker/InternalServiceStartException.java
+++ src/main/java/com/magentamause/cosybackend/exceptions/docker/InternalServiceStartException.java
@@ -1,6 +1,5 @@
 package com.magentamause.cosybackend.exceptions.docker;
 
-import com.github.dockerjava.api.exception.InternalServerErrorException;
 import lombok.Getter;
 
 public class InternalServiceStartException extends Exception {
