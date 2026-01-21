0f9bbbda82941fd3c923f40562a3bed51cd5bb1c
py-bay <github.com.tribunal325@passmail.net>
2026-01-21 21:44:21 +0100

    chore: remove unused DockerMappingUtils utility class

---
+++ src/main/java/com/magentamause/cosybackend/services/engine/docker/util/DockerStatsMapper.java
@@ -0,0 +1,13 @@
+package com.magentamause.cosybackend.services.engine.docker.util;
+
+import com.magentamause.cosybackend.dtos.entitydtos.GameServerDto;
+
+public class DockerStatsMapper {
+
+    public static GameServerDto.GameServerStatus mapDockerStateToGameServerStatus(String dockerState) {
+        return switch (dockerState != null ? dockerState.toLowerCase() : "") {
+            case "running" -> GameServerDto.GameServerStatus.RUNNING;
+            default -> GameServerDto.GameServerStatus.STOPPED;
+        };
+    }
+}
