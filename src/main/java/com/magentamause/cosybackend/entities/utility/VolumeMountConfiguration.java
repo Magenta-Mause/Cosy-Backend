437952e91417642384cb1ee692d2d7520e354650
Janne Keipert <jabbekeipert@gmail.com>
2026-01-27 11:11:52 +0100

    fix: bring temporarely hostpath back to backend

--- src/main/java/com/magentamause/cosybackend/entities/utility/VolumeMountConfiguration.java
+++ src/main/java/com/magentamause/cosybackend/entities/utility/VolumeMountConfiguration.java
@@ -18,6 +18,10 @@ public class VolumeMountConfiguration {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String uuid;
 
+    // TODO: Remove as soon as HostPath isnt used in the frontend anymore
+    @Column(nullable = false)
+    private String hostPath;
+
     @Column(nullable = false)
     private String containerPath;
 
