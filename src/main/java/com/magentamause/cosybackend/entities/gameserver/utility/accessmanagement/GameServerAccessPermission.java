a0ce6fb5f7baebcb51cec43ecbd58f1517d7d8f4
Janne Keipert <jabbekeipert@gmail.com>
2026-02-15 15:56:47 +0100

    feat: update access permissions enum for game server management
    - Moved `START_STOP_SERVER` to align with base permissions for better organization.

--- src/main/java/com/magentamause/cosybackend/entities/gameserver/utility/accessmanagement/GameServerAccessPermission.java
+++ src/main/java/com/magentamause/cosybackend/entities/gameserver/utility/accessmanagement/GameServerAccessPermission.java
@@ -6,16 +6,17 @@ public enum GameServerAccessPermission {
     // Base
     SEE_SERVER,
 
+    START_STOP_SERVER,
+
     // Logs/Metrics
     READ_SERVER_LOGS,
     READ_SERVER_METRICS,
+
 
     // File
     READ_SERVER_SERVER_FILES,
     CHANGE_SERVER_FILES,
 
-    // Server Actions
-    START_STOP_SERVER,
     SEND_COMMANDS,
 
     // Configs
