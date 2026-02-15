7f5a743aa403d676a340bddca6088227142e28a8
simon dietrich <github.difficult926@passmail.net>
2026-02-15 20:27:04 +0100

    refactor: rename GameServerWebhookRepository to WebhookRepository

---
+++ src/main/java/com/magentamause/cosybackend/repositories/WebhookRepository.java
@@ -0,0 +1,12 @@
+package com.magentamause.cosybackend.repositories;
+
+import com.magentamause.cosybackend.entities.WebhookEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebhookRepository
+        extends JpaRepository<WebhookEntity, String> {
+    List<WebhookEntity> findByGameServer_Uuid(String gameServerUuid);
+
+    long deleteByUuidAndGameServer_Uuid(String webhookId, String gameServerUuid);
+}
