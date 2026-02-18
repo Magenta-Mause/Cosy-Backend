1f7dc2d6f4b53bd99369d63c2320baa85685a2de
Janne Keipert <jabbekeipert@gmail.com>
2026-02-18 09:39:07 +0100

    fix: update `InternalGameServerController` to use Authorization header for secret handling
    - Replaced `secret` as a path variable withconnection` endpoints for improved security.

--- src/main/java/com/magentamause/cosybackend/controllers/gameserver/InternalGameServerController.java
+++ src/main/java/com/magentamause/cosybackend/controllers/gameserver/InternalGameServerController.java
@@ -1,7 +1,9 @@
 package com.magentamause.cosybackend.controllers.gameserver;
 
 import com.magentamause.cosybackend.services.core.gameserver.GameServerService;
+
 import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +17,17 @@ public class InternalGameServerController {
 
     private final GameServerService gameServerService;
 
-    @PutMapping("/custom-metric/{uuid}/{secret}")
+    @PutMapping("/custom-metric/{uuid}")
     public ResponseEntity<Map<String, Object>> updateCustomMetric(
             @PathVariable String uuid,
-            @PathVariable String secret,
+            @RequestHeader("Authorization") String secret,
             @RequestBody Map<String, Object> value) {
         return ResponseEntity.ok(gameServerService.updateCustomMetric(uuid, secret, value));
     }
 
-    @GetMapping("/test-connection/{uuid}/{secret}")
+    @GetMapping("/test-connection/{uuid}")
     public ResponseEntity<Boolean> checkConnection(
-            @PathVariable String uuid, @PathVariable String secret) {
+            @PathVariable String uuid, @RequestHeader("Authorization") String secret) {
         return ResponseEntity.ok(gameServerService.checkGameServerConnection(uuid, secret));
     }
 }
