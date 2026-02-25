97bb89b0a357efb195497d7ba05c1f3eedf902b8
Alexander <106274860+EcoFreshKase@users.noreply.github.com>
2026-02-25 16:25:15 +0100

    feat: rename default admin to owner (#85)

--- src/main/java/com/magentamause/cosybackend/configs/properties/DefaultProperties.java
+++ src/main/java/com/magentamause/cosybackend/configs/properties/DefaultProperties.java
@@ -3,7 +3,7 @@ package com.magentamause.cosybackend.configs.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "cosy.defaults")
-public record DefaultProperties(Admin admin) {
+public record DefaultProperties(Owner owner) {
 
-    public record Admin(String username, String password) {}
+    public record Owner(String username, String password) {}
 }
