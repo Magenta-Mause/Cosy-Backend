47d767360b9b8e1123e63802b93ed6e83e10979e
MCLemuri <elefine@mail.de>
2026-03-02 13:17:55 +0100

    lint fix

--- src/main/java/com/magentamause/cosybackend/entities/gameserver/GameServerEntity.java
+++ src/main/java/com/magentamause/cosybackend/entities/gameserver/GameServerEntity.java
@@ -126,7 +126,10 @@ public class GameServerEntity {
                 .createdOn(this.getCreatedOn())
                 .timestampLastStarted(this.getTimestampLastStarted())
                 .gameUuid(Optional.ofNullable(this.getGame()).map(GameEntity::getUuid).orElse(null))
-                .externalGameId(Optional.ofNullable(this.getGame()).map(GameEntity::getExternalGameId).orElse(null))
+                .externalGameId(
+                        Optional.ofNullable(this.getGame())
+                                .map(GameEntity::getExternalGameId)
+                                .orElse(null))
                 .rconConfiguration(this.getRconConfiguration())
                 .dockerImageName(this.getDockerImageName())
                 .dockerImageTag(this.getDockerImageTag())
@@ -161,7 +164,10 @@ public class GameServerEntity {
         return GameServerDto.builder()
                 .uuid(this.getUuid())
                 .gameUuid(Optional.ofNullable(this.getGame()).map(GameEntity::getUuid).orElse(null))
-                .externalGameId(Optional.ofNullable(this.getGame()).map(GameEntity::getExternalGameId).orElse(null))
+                .externalGameId(
+                        Optional.ofNullable(this.getGame())
+                                .map(GameEntity::getExternalGameId)
+                                .orElse(null))
                 .serverName(this.getServerName())
                 .status(this.getStatus())
                 .owner(Optional.ofNullable(this.getOwner()).map(UserEntity::toDto).orElse(null))
