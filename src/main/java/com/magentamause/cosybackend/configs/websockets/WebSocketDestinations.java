9f46dfa7e2953c7efeb242e90f25a86400f01baf
Janne <60547730+Janne6565@users.noreply.github.com>
2026-03-03 10:46:32 +0100

    refactor: update WebSocket destination paths for game server topics (#94)

--- src/main/java/com/magentamause/cosybackend/configs/websockets/WebSocketDestinations.java
+++ src/main/java/com/magentamause/cosybackend/configs/websockets/WebSocketDestinations.java
@@ -12,14 +12,14 @@ public final class WebSocketDestinations {
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static final class Topics {
-        public static final String GAME_SERVER_LOGS_CREATION =
-                BROKER_PREFIX + "/game-server-logs/creation/{serverId}";
+        public static final String GAME_SERVER_LOGS =
+                BROKER_PREFIX + "/game-servers/{serverId}/logs";
+        public static final String GAME_SERVER_METRICS =
+                BROKER_PREFIX + "/game-servers/{serverId}/metrics";
         public static final String GAME_SERVER_UPDATES =
                 BROKER_PREFIX + "/game-servers/updates/{serverId}";
         public static final String GAME_SERVER_DOCKER_PROGRESS =
                 BROKER_PREFIX + "/game-servers/docker-progress/{serverId}";
-        public static final String GAME_SERVER_METRICS =
-                BROKER_PREFIX + "/game-servers/metrics/{serverId}";
         public static final String GAME_SERVER_PERMISSIONS_CONFIG_CHANGE =
                 BROKER_PREFIX + "/game-servers/permissions-config-change/{userId}";
     }
