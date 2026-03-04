441486946090607f7a2e7854f6a8d09e50d59117
Janne <jabbekeipert@gmail.com>
2026-03-04 14:20:56 +0100

    hotfix: increase WebClient memory size limit for Loki requests

--- src/main/java/com/magentamause/cosybackend/configs/WebClientConfig.java
+++ src/main/java/com/magentamause/cosybackend/configs/WebClientConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -31,8 +32,13 @@ public class WebClientConfig {
                                 (lokiProperties.username() + ":" + lokiProperties.password())
                                         .getBytes());
 
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
+                .build();
+
         return WebClient.builder()
                 .uriBuilderFactory(factory)
+                .exchangeStrategies(strategies)
                 .defaultHeaders(
                         headers -> {
                             headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
